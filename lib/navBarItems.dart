81691d103a795e346fa245e97e1c2dc497f2b94d
simon-jo8 <112388673+simon-jo8@users.noreply.github.com>
2023-10-27 15:01:13 +0200

    ;

---
+++ lib/navBarItems.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
+
+class navBarItem{
+  final String? label;
+  final String? route;
+  final IconData icon;
+  const navBarItem({this.label, this.route, required this.icon});
+}
--- lib/ultimate_navigation_bar.dart
+++ lib/ultimate_navigation_bar.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:ultimate_navigation_bar/navBarItems.dart';
 
 class ultimateNavBar extends StatelessWidget {
-  const ultimateNavBar({Key? key}) : super(key: key);
+  final List<navBarItem>? items;
+  const ultimateNavBar({Key? key, required this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Ultimate Navigation Bar'),
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          for (var item in items!)
+             Container(
+                height: 60,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(item.icon),
+                    Text(item.label!),
+                  ],
+                ),
+              ),
+        ],
+      ),
     );
   }
 }
